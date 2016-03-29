46eb3131eff22e07646bdab131595983af5af46d
yuanwen <wenshin2011@gmail.com>
2016-03-29 13:16:26 +0800

    Update: 美团App 测试用例

--- test/index.js
+++ test/index.js
@@ -3,10 +3,16 @@ import useragents from './utils/useragents';
 import seeagent from '../lib';
 
 describe('seeagent', () => {
-  it('parsed all useragents right', () => {
+  it('should parsed all useragents right', () => {
     for (const item of useragents) {
-      let agent = seeagent(item.userAgent);
+      let agent = seeagent({query: item.query, httpUserAgent: item.userAgent});
       assert.deepEqual(agent, item.parsed);
     }
   });
+
+  // it('can extend fingerprints', () => {
+  // });
+
+  // it('can extend query rules', () => {
+  // });
 });
--- test/utils/useragents.js
+++ test/utils/useragents.js
@@ -9,14 +9,43 @@ export default [{
   userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_2_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13D15 Safari/601.1',
   parsed: {
     os: { family: 'iOS', major: '9', minor: '2', patch: '1' },
-    device: { family: 'iPhone', major: '0', minor: '0', patch: '0'},
-    app: {family: 'Mobile Safari', major: '9', minor: '0', patch: '0'},
+    device: { family: 'iPhone', major: '0', minor: '0', patch: '0' },
+    app: { family: 'Mobile Safari', major: '9', minor: '0', patch: '0' },
   }
 }, {
-  userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_2_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13D15 MicroMessenger/5.0.1',
+  userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_2_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13D15 MicroMessenger/6.3.9 NetType/WIFI Language/zh_CN',
   parsed: {
     os: { family: 'iOS', major: '9', minor: '2', patch: '1' },
-    device: { family: 'iPhone', major: '0', minor: '0', patch: '0'},
-    app: {family: 'Wechat', major: '5', minor: '0', patch: '1'}
+    device: { family: 'iPhone', major: '0', minor: '0', patch: '0' },
+    app: { family: 'Wechat', major: '6', minor: '3', patch: '9' }
+  }
+}, {
+  userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_2_1 like Mac OS X) AppleWebKit/601.1 (KHTML, like Gecko) CriOS/48.0.2564.104 Mobile/13D15 Safari/601.1.46',
+  parsed: {
+    os: { family: 'iOS', major: '9', minor: '2', patch: '1' },
+    device: { family: 'iPhone', major: '0', minor: '0', patch: '0' },
+    app: { family: 'Chrome Mobile iOS', major: '48', minor: '0', patch: '2564' }
+  }
+}, {
+  userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_2_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13D15',
+  query: 'AgroupB',
+  parsed: {
+    os: { family: 'iOS', major: '9', minor: '2', patch: '1' },
+    device: { family: 'iPhone', major: '0', minor: '0', patch: '0' },
+    app: { family: 'Meituan', major: '0', minor: '0', patch: '0' }
+  }
+}, {
+  userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_2_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13D15 movie/6.5/509 MTNB/1.0.0 (iOS; Maoyan 509)',
+  parsed: {
+    os: { family: 'iOS', major: '9', minor: '2', patch: '1' },
+    device: { family: 'iPhone', major: '0', minor: '0', patch: '0' },
+    app: { family: 'Maoyan', major: '0', minor: '0', patch: '0' }
+  }
+}, {
+  userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) daxiang-app/1.0.0 Chrome/43.0.2357.65 Electron/0.30.4 Safari/537.36',
+  parsed: {
+    os: { family: 'iOS', major: '9', minor: '2', patch: '1' },
+    device: { family: 'iPhone', major: '0', minor: '0', patch: '0' },
+    app: { family: 'Daxiang', major: '1', minor: '0', patch: '0' }
   }
 }];
