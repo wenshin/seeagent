fca53d79ea290bae98075e79a6cd47256de92d28
yuanwen <wenshin2011@gmail.com>
2016-05-20 12:12:18 +0800

    Add: add device type parser

--- test/useragents/pc.js
+++ test/useragents/pc.js
@@ -2,7 +2,7 @@ module.exports = [{
   userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36',
   parsed: {
     os: { family: 'Mac OS X', major: '10', minor: '11', patch: '3' },
-    device: { family: 'Other', major: '0', minor: '0', patch: '0' },
+    device: { family: 'Other', major: '0', minor: '0', patch: '0', type: 'desktop' },
     app: { family: 'Chrome', major: '49', minor: '0', patch: '2623' }
   }
 }, {
@@ -17,21 +17,21 @@ module.exports = [{
   userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_2_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13D15 Safari/601.1',
   parsed: {
     os: { family: 'iOS', major: '9', minor: '2', patch: '1' },
-    device: { family: 'iPhone', major: '0', minor: '0', patch: '0' },
+    device: { family: 'iPhone', major: '0', minor: '0', patch: '0', type: 'phone' },
     app: { family: 'Mobile Safari', major: '9', minor: '0', patch: '0' },
   }
 }, {
   userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_2_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13D15 MicroMessenger/6.3.9 NetType/WIFI Language/zh_CN',
   parsed: {
     os: { family: 'iOS', major: '9', minor: '2', patch: '1' },
-    device: { family: 'iPhone', major: '0', minor: '0', patch: '0' },
+    device: { family: 'iPhone', major: '0', minor: '0', patch: '0', type: 'phone' },
     app: { family: 'Wechat', major: '6', minor: '3', patch: '9' }
   }
 }, {
   userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_2_1 like Mac OS X) AppleWebKit/601.1 (KHTML, like Gecko) CriOS/48.0.2564.104 Mobile/13D15 Safari/601.1.46',
   parsed: {
     os: { family: 'iOS', major: '9', minor: '2', patch: '1' },
-    device: { family: 'iPhone', major: '0', minor: '0', patch: '0' },
+    device: { family: 'iPhone', major: '0', minor: '0', patch: '0', type: 'phone' },
     app: { family: 'Chrome Mobile iOS', major: '48', minor: '0', patch: '2564' }
   }
 }, {
@@ -39,21 +39,21 @@ module.exports = [{
   query: 'AgroupB',
   parsed: {
     os: { family: 'iOS', major: '9', minor: '2', patch: '1' },
-    device: { family: 'iPhone', major: '0', minor: '0', patch: '0' },
+    device: { family: 'iPhone', major: '0', minor: '0', patch: '0', type: 'phone' },
     app: { family: 'Meituan', major: '0', minor: '0', patch: '0' }
   }
 }, {
   userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_2_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13D15 movie/6.5/509 MTNB/1.0.0 (iOS; Maoyan 509)',
   parsed: {
     os: { family: 'iOS', major: '9', minor: '2', patch: '1' },
-    device: { family: 'iPhone', major: '0', minor: '0', patch: '0' },
+    device: { family: 'iPhone', major: '0', minor: '0', patch: '0', type: 'phone' },
     app: { family: 'Maoyan', major: '6', minor: '5', patch: '0' }
   }
 }, {
   userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) daxiang-app/1.0.0 Chrome/43.0.2357.65 Electron/0.30.4 Safari/537.36',
   parsed: {
     os: { family: 'Mac OS X', major: '10', minor: '11', patch: '3' },
-    device: { family: 'Other', major: '0', minor: '0', patch: '0' },
+    device: { family: 'Other', major: '0', minor: '0', patch: '0', type: 'desktop' },
     app: { family: 'Daxiang', major: '1', minor: '0', patch: '0' }
   }
 }];
